3f4ca73935ad1febb563b751ba6107a6834de8b8
jake rudlong <jrudlong@gmail.com>
2023-05-28 21:09:54 -0500

    removse vite icon from index

--- vite.config.js
+++ vite.config.js
@@ -5,13 +5,4 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   base: "/nosh-it-delivery",
-  build: {
-    rollupOptions: {
-      // ...
-      external: [
-        // ...
-        'react-icons/Gr',
-      ],
-    },
-  },
 })
