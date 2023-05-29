a969efe3109d3e31510c49b1fc4617289dd36172
jake rudlong <jrudlong@gmail.com>
2023-05-28 20:42:39 -0500

    edits gr icons

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,14 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/nosh-it-delivery"
+  base: "/nosh-it-delivery",
+  build: {
+    rollupOptions: {
+      // ...
+      external: [
+        // ...
+        'react-icons/Gr',
+      ],
+    },
+  },
 })
